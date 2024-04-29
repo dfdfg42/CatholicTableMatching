27473266ad85abb72c5f3c9c2847dcb9a1f55331
고경우 <https://github.com/dfdfg42>
2024-04-29 12:56:53 +0900

    start

---
+++ src/main/java/com/csec/CatholicTableMatching/CatholicTableMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.csec.CatholicTableMatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatholicTableMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatholicTableMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csec/CatholicTableMatching/CatholicTableMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csec.CatholicTableMatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatholicTableMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
